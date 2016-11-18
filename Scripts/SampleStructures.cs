76b271b6114319a40c5b5fa1a21f825ef4b4facd
deon9718 <chunqiz@uci.edu>
2016-11-17 22:19:35 -0800

    Going to 2D
    Removed Nature Pack
    Added Kenny's Medieval RTS pack Tile Sheet
    Created a basic scene
    Added unit highlighting system

---
+++ Scripts/SampleStructures.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class SampleStructures : Unit
+{
+    public Color LeadingColor;
+    public override void Initialize()
+    {
+        base.Initialize();
+        transform.position += new Vector3(0, 0, -1);
+        GetComponentInChildren<Renderer>().material.color = LeadingColor;
+    }
+    public override void MarkAsAttacking(Unit other)
+    {
+    }
+
+    public override void MarkAsDefending(Unit other)
+    {
+    }
+
+    public override void MarkAsDestroyed()
+    {
+    }
+
+    public override void MarkAsFinished()
+    {
+    }
+
+    public override void MarkAsFriendly()
+    {
+        //GetComponent<Renderer>().material.color = LeadingColor + new Color(1, 1, 1);
+    }
+
+    public override void MarkAsReachableEnemy()
+    {
+        //GetComponent<Renderer>().material.color = LeadingColor + Color.red ;
+    }
+
+    public override void MarkAsSelected()
+    {
+        //GetComponent<Renderer>().material.color = LeadingColor + Color.green;
+    }
+
+    public override void UnMark()
+    {
+        //GetComponent<Renderer>().material.color = LeadingColor;
+    }
+}
---
+++ Scripts/SampleUnit.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class SampleUnit : Unit
+{
+    public Color LeadingColor;
+    public SpriteRenderer highlight;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+        transform.position += new Vector3(0, 0, -1);
+        highlight = transform.FindChild("Man_Highlight").GetComponent<SpriteRenderer>();
+        highlight.enabled = false;
+        //GetComponentInChildren<Renderer>().material.color = LeadingColor;
+    }
+    public override void MarkAsAttacking(Unit other)
+    {      
+    }
+
+    public override void MarkAsDefending(Unit other)
+    {       
+    }
+
+    public override void MarkAsDestroyed()
+    {      
+    }
+
+    public override void MarkAsFinished()
+    {
+        highlight.color = new Color(0, 0, 0, 0.5f);
+        highlight.enabled = true;
+    }
+
+    public override void MarkAsFriendly()
+    {
+        highlight.color = new Color(0, 1, 0, 0.5f);
+        highlight.enabled = true;
+    }
+
+    public override void MarkAsReachableEnemy()
+    {
+        highlight.color = new Color(0, 0, 1, 0.5f);
+        highlight.enabled = true;
+    }
+
+    public override void MarkAsSelected()
+    {
+        highlight.color = new Color(1, 1, 0, 0.5f);
+        highlight.enabled = true;
+    }
+
+    public override void UnMark()
+    {
+        highlight.enabled = false;
+    }
+}
